297a94b65ef4d839ddacef0e43ceae5009a4c1e8
Maheshwarreddy1 <61952430+Maheshwarreddy1@users.noreply.github.com>
2021-09-11 18:38:25 +0530

    testing completed

--- collebra_task.py
+++ collebra_task.py
@@ -6,4 +6,4 @@ print(response.json())
 b={'vendorDetails': {'oui': '443839', 'isPrivate': False, 'companyName': 'Cumulus Networks, Inc', 'companyAddress': '650 Castro Street, suite 120-245 Mountain View CA 94041 US', 'countryCode': 'US'}, 'blockDetails': {'blockFound': True, 'borderLeft': '443839000000', 'borderRight': '443839FFFFFF', 'blockSize': 16777216, 'assignmentBlockSize': 'MA-L', 'dateCreated': '2012-04-08', 'dateUpdated': '2015-09-27'}, 'macAddressDetails': {'searchTerm': '44:38:39:ff:ef:57', 'isValid': True, 'virtualMachine': 'Not detected', 'applications': ['Multi-Chassis Link Aggregation (Cumulus Linux)'], 'transmissionType': 'unicast', 'administrationType': 'UAA', 'wiresharkNotes': 'No details', 'comment': ''}}
 print(b["macAddressDetails"]["searchTerm"])
 #print(b['vendorDetails']['companyName'])
-#completed task
+#done
